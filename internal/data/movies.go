a3e2831cb6825e71ed0b04acaaf420191d0e24b4
Cerecero <ale.rey.cer@outlook.com>
2025-02-25 16:56:27 -0600

    updated movie struct

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Movie struct {
-	ID        int64 `json:"id"`
-	CreatedAt time.Time`json:"created_at"`
-	Title     string`json:"title"`
-	Year      int32`json:"year"`
-	Runtime   int32`json:"runtime"`
-	Genres    []string`json:"genres"`
-	Version   int32`json:"version"`
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"` //- to ommit in the json response
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"` // omitempty directive, use json:",omitempty" if you want to keep the key name but omit if empty
+	Runtime   int32     `json:"runtime,omitempty""`
+	Genres    []string  `json:"genres,omitempty""`
+	Version   int32     `json:"version"`
 }
