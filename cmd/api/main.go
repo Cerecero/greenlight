68ac998f4d98bf910ecae6a0b685f4344154098b
Cerecero <ale.rey.cer@outlook.com>
2025-02-27 12:19:15 -0600

    added confi options to the db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,8 +19,11 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
-	db struct {
-		dsn string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
 	}
 }
 
@@ -42,6 +45,10 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", dbURL, "PostgresSQL DSN")
 
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgresSQL max open connection")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgresSQL max idle connection")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15mins", "PostgresSQL max connection idle time")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -61,7 +68,7 @@ func main() {
 
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:     app.routes(),
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
@@ -79,6 +86,16 @@ func openDB(cfg config) (*sql.DB, error) {
 		return nil, err
 	}
 
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -89,5 +106,3 @@ func openDB(cfg config) (*sql.DB, error) {
 	return db, nil
 
 }
-
-
