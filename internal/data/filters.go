08147598a736744e227b1de5a387f932aa133c28
Cerecero <ale.rey.cer@outlook.com>
2025-03-03 15:41:22 -0600

    added pagination

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,34 @@
 package data
 
-import "github.com/cerecero/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/cerecero/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page     int
 	PageSize int
 	Sort     string
 	SortSafelist []string
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
@@ -16,4 +38,12 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
