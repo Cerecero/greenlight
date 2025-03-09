df60bf8fb2338d393e095b111fa7a7b91f89639e
Cerecero <ale.rey.cer@outlook.com>
2025-03-09 17:37:39 -0600

    finished authentication with handlers

--- internal/data/users.go
+++ internal/data/users.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cerecero/greenlight/internal/validator"
 	"golang.org/x/crypto/bcrypt"
 )
+var AnonymousUser = &User{}
 
 type User struct {
 	ID        int64     `json:"id"`
@@ -19,6 +20,10 @@ type User struct {
 	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 type password struct {
