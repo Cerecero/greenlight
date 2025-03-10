984a6f4957860ce6a304d2f78b33dc05ecc4ec72
Cerecero <ale.rey.cer@outlook.com>
2025-03-10 16:06:12 -0600

    finished permissionBasedAUth

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -59,6 +59,12 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	err = app.models.Permissions.AddForUser(user.ID, "movies:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
 		data := map[string]interface{}{
 			"activationToken": token.Plaintext,
--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Permissions []string
- 
+
 func (p Permissions) Include(code string) bool {
 	for i := range p {
 		if code == p[i] {
@@ -55,4 +57,15 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+        INSERT INTO users_permissions
+        SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
