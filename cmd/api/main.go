4229fc643a5168d0e02609cd7268816c192b058e
Cerecero <ale.rey.cer@outlook.com>
2025-03-11 11:19:03 -0600

    added a new flag for trusted cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -77,6 +81,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", smtpName, "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", smtpPass, "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenligh.cerecero.com>", "SMTP sneder")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
